8f0e2930a136e5faf9296e05fabd297ff64fbf9e
mhieu1296 <tkthu3cuatoi@gmail.com>
2025-04-21 21:39:38 +0700

    tai cau truc lai thu muc

--- js/hien_thi_LED.js
+++ js/hien_thi_LED.js
@@ -1,29 +1,3 @@
-console.log(
-  "%cTRANSERCO HANOIBUS",
-  "COLOR: GREEN; font-size: 20px; font-family: COURIER"
-);
-
-console.log(
-  "%cXin chào! 🚏🚌",
-  "color: red; font-size: 50px; font-weight: bold; font-family: tahoma"
-);
-console.log(
-  "%cLED xe buýt xin chào những người yêu xe buýt Hà Nội và Tổng Công ty Vận tải Hà Nội! Bạn có thể thao tác mọi thứ trên Dev Tools.",
-  "font-size: 20px; font-family: Arial"
-);
-
-function daoChieuLED(Dau, Cuoi) {
-  // đảo điểm đầu và điểm cuối
-  let diemDau = document.getElementById(Dau);
-  let diemCuoi = document.getElementById(Cuoi);
-  if (diemDau && diemCuoi) {
-    let temp = diemDau.innerText;
-    diemDau.innerText = diemCuoi.innerText;
-    diemCuoi.innerText = temp;
-    console.log("Đã đổi thành công");
-  }
-}
-
 let running = false;
 
 function getElements() {
@@ -341,7 +315,6 @@ function hienThiMaTuyen_Transerco() {
     }
   }, 5000);
 }
-
 function startToggle() {
   // Hàm chạy LED
   if (running) return; // Nếu đã chạy thì không khởi động lại
@@ -418,66 +391,4 @@ function reset() {
   HuyDong.style.display = "none";
   VeGara.style.display = "none";
   console.log("Xóa LED thành công");
-}
-
-function NhapDuLieu() {
-  // Lấy tất cả các input trong div NhapDuLieu
-  const inputs = document.querySelectorAll(".NhapDuLieu input");
-
-  // Kiểm tra xem có ô nào bị bỏ trống không
-  for (let input of inputs) {
-    if (input.value.trim() === "") {
-      alert(
-        "Vui lòng nhập đầy đủ Mã tuyến, điểm đầu, điểm cuối và tên xí nghiệp."
-      );
-      return;
-    }
-  }
-
-  // Gán giá trị từ input vào các thẻ <p> tương ứng
-  const maTuyen = inputs[0].value;
-  const diemDau = inputs[1].value;
-  const diemCuoi = inputs[2].value;
-  const xiNghiep = inputs[3].value;
-
-  document.getElementById("maTuyen").innerText = maTuyen;
-  document.getElementById("diemDau").innerText = diemDau;
-  document.getElementById("diemCuoi").innerText = diemCuoi;
-  document.getElementById("xiNghiep").innerText = xiNghiep;
-  document.getElementById("MaTuyenCanGiua").innerText = maTuyen;
-
-  // Lưu vào localStorage
-  localStorage.setItem("maTuyen", maTuyen);
-  localStorage.setItem("diemDau", diemDau);
-  localStorage.setItem("diemCuoi", diemCuoi);
-  localStorage.setItem("xiNghiep", xiNghiep);
-  localStorage.setItem("MaTuyenCanGiua", maTuyen);
-  console.log("Đã nhập đầy đủ thông tin.");
-}
-
-window.onload = function () {
-  // xóa toàn bộ LED và dừng toàn bộ hoạt động LED khi tải xong trang, sẽ kích hoạt khi chạy hàm startToggle();
-  stopToggle();
-  reset();
-
-  // nhớ lại local storage
-  const maTuyen = localStorage.getItem("maTuyen");
-  const diemDau = localStorage.getItem("diemDau");
-  const diemCuoi = localStorage.getItem("diemCuoi");
-  const xiNghiep = localStorage.getItem("xiNghiep");
-
-  if (maTuyen) document.getElementById("maTuyen").innerText = maTuyen;
-  if (diemDau) document.getElementById("diemDau").innerText = diemDau;
-  if (diemCuoi) document.getElementById("diemCuoi").innerText = diemCuoi;
-  if (xiNghiep) {
-    document.getElementById("xiNghiep").innerText = xiNghiep;
-    document.getElementById("MaTuyenCanGiua").innerText = maTuyen;
-  }
-};
-
-function underConstruction() {
-  const button = document.getElementsByClassName("TangCuongTuyen");
-  button[0].addEventListener("click", function () {
-    alert("Tính năng đang được xây dựng");
-  });
 }
