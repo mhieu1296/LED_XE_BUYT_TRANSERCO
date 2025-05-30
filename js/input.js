41247c18e1fa2139302b920b197f9ce705e6d730
mhieu1296 <tkthu3cuatoi@gmail.com>
2025-05-31 01:33:29 +0700

    Them diem giua, danh cho cac tuyen co 3 chang

--- js/input.js
+++ js/input.js
@@ -1,25 +1,25 @@
 function NhapDuLieu() {
-  // Lấy tất cả các input trong div NhapDuLieu
-  const inputs = document.querySelectorAll(".NhapDuLieu input");
-  // Kiểm tra xem có ô nào bị bỏ trống không
+  const inputs = document.querySelectorAll("input");
+
   for (let input of inputs) {
-    if (input.value.trim() === "") {
-      document.getElementById("canhBao").innerText =
-        "HÃY NHẬP ĐẦY ĐỦ CÁC TRƯỜNG!";
+    const isRequired = !input.classList.contains("khongBatBuoc");
+
+    if (isRequired && input.value.trim() === "") {
+      document.getElementById("canhBao").innerText = "NHẬP ĐỦ CÁC TRƯỜNG CÓ * !";
       return;
-    } else {
-      document.getElementById("canhBao").innerText = "ĐÃ NHẬP ĐỦ";
     }
   }
 
   // Gán giá trị từ input vào localDrage để gán vào khi load trang (window_onload.js)
   const maTuyen = inputs[0].value;
   const diemDau = inputs[1].value;
-  const diemCuoi = inputs[2].value;
-  const xiNghiep = inputs[3].value;
+  const diemGiua = inputs[2].value;
+  const diemCuoi = inputs[3].value;
+  const xiNghiep = inputs[4].value;
 
   localStorage.setItem("maTuyen", maTuyen);
   localStorage.setItem("diemDau", diemDau);
+  localStorage.setItem("diemGiua", diemGiua);
   localStorage.setItem("diemCuoi", diemCuoi);
   localStorage.setItem("xiNghiep", xiNghiep);
   localStorage.setItem("MaTuyenCanGiua", maTuyen);
--- js/window_onload.js
+++ js/window_onload.js
@@ -8,17 +8,34 @@ window.onload = function () {
   // nhớ lại local storage
   const maTuyen = localStorage.getItem("maTuyen");
   const diemDau = localStorage.getItem("diemDau");
+  const diemGiua = localStorage.getItem("diemGiua");
   const diemCuoi = localStorage.getItem("diemCuoi");
   const xiNghiep = localStorage.getItem("xiNghiep");
 
+  console.log(diemGiua);
+
   document.getElementById("maTuyen").innerText = maTuyen;
   document.getElementById("diemDau").innerText = diemDau;
+  // document.getElementById("diemGiua").innerText = diemGiua;
+  if (diemGiua && diemGiua.trim() !== "") {
+    document.getElementById("diemGiua").innerText = diemGiua;
+  } else {
+    document.getElementById("mui_ten_xanh2").style.display = "none";
+    document.getElementById("diemGiua").style.display = "none";
+  }
   document.getElementById("diemCuoi").innerText = diemCuoi;
   document.getElementById("xiNghiep").innerText = xiNghiep;
   document.getElementById("MaTuyenCanGiua").innerText = maTuyen;
 
   document.getElementById("maTuyenLuu").innerText = maTuyen;
   document.getElementById("diemDauLuu").innerText = diemDau;
+  // document.getElementById("diemGiuaLuu").innerText = diemGiua;
+    if (diemGiua && diemGiua.trim() !== "") {
+    document.getElementById("diemGiuaLuu").innerText = diemGiua;
+  } else {
+    document.getElementById("mui_ten1").style.display = "none";
+    document.getElementById("diemGiuaLuu").style.display = "none";
+  }
   document.getElementById("diemCuoiLuu").innerText = diemCuoi;
   document.getElementById("xiNghiepLuu").innerText = xiNghiep;
 };
